b574c41f5f8cddbf3ac60b642e56d2e99d6921d6
Daniel Miller <daniel.miller@coxautoinc.com>
2017-11-17 08:54:45 -0500

    Update formsy

--- src/hoc/component.js
+++ src/hoc/component.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {Wrapper as FormsyHOC} from 'formsy-react';
+import {withFormsy as FormsyHOC} from 'formsy-react';
 import styleClassNames from '../components/prop-types';
 
 function getDisplayName(component) {
